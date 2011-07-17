642d6f298cc0ffbdb426a225a6c1815dd6293d6c
cjbooms <cjbooms@e16e9eb0-2669-411f-a76c-d7e706733225>
2011-07-17 20:09:57 +0000

    Adding Edit Area

---
+++ trunk/PythonAppEngine/src/main/java/com/gmail/cjbooms/thesis/pythonappengine/client/FileSystemServiceAsync.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gmail.cjbooms.thesis.pythonappengine.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ *
+ * @author firas
+ */
+public interface FileSystemServiceAsync {
+    void getContents(FileWrapper file, AsyncCallback<FileWrapper[]> callback);
+    void getContents(String file, AsyncCallback<FileWrapper[]> callback);
+    void deleteFile(String absoluteName, AsyncCallback<Boolean> callback);
+}
