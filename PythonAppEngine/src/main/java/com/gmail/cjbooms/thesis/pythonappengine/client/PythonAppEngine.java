14e7fe538ac616834a0cf79ce181542d3b0511f6
cjbooms <cjbooms@gmail.com>
2012-03-21 00:11:10 +0000

    Adding copyright to all files

--- PythonAppEngine/src/main/java/com/gmail/cjbooms/thesis/pythonappengine/client/PythonAppEngine.java
+++ PythonAppEngine/src/main/java/com/gmail/cjbooms/thesis/pythonappengine/client/PythonAppEngine.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2012 Conor Gallagher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.gmail.cjbooms.thesis.pythonappengine.client;
 
 import com.gmail.cjbooms.thesis.pythonappengine.client.editor.SourceCodeEditor;
@@ -5,15 +25,9 @@ import com.gmail.cjbooms.thesis.pythonappengine.client.filebrowser.FileSystemTre
 import com.gmail.cjbooms.thesis.pythonappengine.client.menus.MainMenuWidget;
 import com.gmail.cjbooms.thesis.pythonappengine.client.menus.Toolbar;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.event.logical.shared.ResizeEvent;
-import com.google.gwt.event.logical.shared.ResizeHandler;
-import com.google.gwt.user.client.*;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.*;
 import com.google.gwt.dom.client.Style.Unit;
-
-import java.awt.*;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.*;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
