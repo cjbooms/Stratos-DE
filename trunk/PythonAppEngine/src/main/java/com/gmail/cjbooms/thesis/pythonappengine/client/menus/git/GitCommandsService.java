d6607bdefbf60ef323eeb7ff01015b54e88df4dd
cjbooms <cjbooms@e16e9eb0-2669-411f-a76c-d7e706733225>
2012-03-21 00:11:10 +0000

    Adding copyright to all files

--- trunk/PythonAppEngine/src/main/java/com/gmail/cjbooms/thesis/pythonappengine/client/menus/git/GitCommandsService.java
+++ trunk/PythonAppEngine/src/main/java/com/gmail/cjbooms/thesis/pythonappengine/client/menus/git/GitCommandsService.java
@@ -1,17 +1,34 @@
 /*
+ * Copyright (c) 2012 Conor Gallagher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 package com.gmail.cjbooms.thesis.pythonappengine.client.menus.git;
 
-import com.gmail.cjbooms.thesis.pythonappengine.client.filebrowser.FileWrapper;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 /**
  *
